90dbf1c44af7adacd00d05f38a20a547e487b595
Tien Han <tienthuyhan@gmail.com>
2024-04-09 15:01:07 -0400

    Turn on error reporting, instantiate Fat-Free, define default route

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * 328/howdy/index.php
+ * Simple MVC using the Fat-Free framework.
+ * @author Tien Han
+ * @version 1.0
+ */
+
+    //Turn on error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+    //Require the autoload file
+    require_once('vendor/autoload.php');
+
+    //Instantiate the F3 Base class (Fat-Free)
+    $f3 = Base::instance();
+
+    //Define a default route
+    $f3-> route('GET /', function() {
+        echo '<p>Howdy!</p>';
+    });
+
+    //Run Fat-Free
+    $f3->run();
+?>
